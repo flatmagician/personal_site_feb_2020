2e7cee4e708f4e8a3f396b15cb5bbf5e81c06038
Ben Taussig <bpt28@cornell.edu>
2020-02-22 10:34:57 -0500

    Updates

---
+++ precache-manifest.883c4869318537d9fa95636383e00a38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe703183eb51e701b8bb38ef6176dcd",
+    "url": "/personal_site_feb_2020/index.html"
+  },
+  {
+    "revision": "5cae170631c625abee35",
+    "url": "/personal_site_feb_2020/static/css/main.70931d59.chunk.css"
+  },
+  {
+    "revision": "2dc0ac5b71e3ae45b771",
+    "url": "/personal_site_feb_2020/static/js/2.441901c0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/personal_site_feb_2020/static/js/2.441901c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cae170631c625abee35",
+    "url": "/personal_site_feb_2020/static/js/main.ded76c58.chunk.js"
+  },
+  {
+    "revision": "3a19a5a01e6fb9951e16",
+    "url": "/personal_site_feb_2020/static/js/runtime-main.002c9951.js"
+  },
+  {
+    "revision": "3dae57a09272091f7ca3819bea52e060",
+    "url": "/personal_site_feb_2020/static/media/11.20___.3dae57a0.TTF"
+  },
+  {
+    "revision": "760eb60525a43e48fb3bc0b740b1cf92",
+    "url": "/personal_site_feb_2020/static/media/BDEject.760eb605.ttf"
+  },
+  {
+    "revision": "e3c40a4c2fce445708ef02d1ff431937",
+    "url": "/personal_site_feb_2020/static/media/FerriteCore-Regular.e3c40a4c.ttf"
+  },
+  {
+    "revision": "c1fd80ed59a0a7baa2987758b376564f",
+    "url": "/personal_site_feb_2020/static/media/FerriteCoreDX-Light.c1fd80ed.ttf"
+  },
+  {
+    "revision": "6402d38ce246649f66547ff38c5406e5",
+    "url": "/personal_site_feb_2020/static/media/PLANK___.6402d38c.TTF"
+  },
+  {
+    "revision": "3853152a844c67a43374d3a605d0de49",
+    "url": "/personal_site_feb_2020/static/media/PLANTC__.3853152a.TTF"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal_site_feb_2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
